3a82a812dade9f242381bc6cbd141cbbc17207e3
jjjjjessica8 <161904431+jjjjjessica8@users.noreply.github.com>
2025-10-08 05:10:28 +1100

    Update otp_utils.py

--- app/auth/otp_utils.py
+++ app/auth/otp_utils.py
@@ -1,1 +1,17 @@
+import pyotp
+import time
+
+def generate_otp_secret():
+    """create a new 2FA"""
+    return pyotp.random_base32()
+
+def get_otp_token(secret):
+    """Generate a 6-digit verification code based on the secret key"""
+    totp = pyotp.TOTP(secret)
+    return totp.now()
+
+def verify_otp_token(secret, token):
+    """Verify whether the verification code entered by the user is correct"""
+    totp = pyotp.TOTP(secret)
+    return totp.verify(token)
 
